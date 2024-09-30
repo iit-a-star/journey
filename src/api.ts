48b564c2d405213007459c75578fc234b71893e2
James Prevett <jp@jamespre.dev>
2024-09-30 14:42:41 -0500

    Fixed crypto?

--- src/api.ts
+++ src/api.ts
@@ -1,14 +1,9 @@
 import type { D1Database } from '@cloudflare/workers-types';
 import { hash } from './utils.js';
-//import { randomBytes } from 'node:crypto';
+import { randomBytes } from 'node:crypto';
 
 function randomHex(length: number): string {
-	//return randomBytes(length).toString('hex');
-	let bytes = '';
-	for (let i = 0; i < length; i++) {
-		bytes += Math.round(Math.random() * 16).toString(16);
-	}
-	return bytes;
+	return randomBytes(length).toString('hex');
 }
 
 let db: D1Database;
